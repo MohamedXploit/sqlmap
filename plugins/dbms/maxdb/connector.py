1bb18c8eadef4a46ab1885ca4195f9339c12ae00
mohamed.cybersec <72762462+MohamedXploit@users.noreply.github.com>
2025-11-27 19:26:55 +0200

    Update error message for SAP HANA connection

--- plugins/dbms/maxdb/connector.py
+++ plugins/dbms/maxdb/connector.py
@@ -10,6 +10,6 @@ from plugins.generic.connector import Connector as GenericConnector
 
 class Connector(GenericConnector):
     def connect(self):
-        errMsg = "on SAP MaxDB it is not (currently) possible to establish a "
+        errMsg = "on SAP HANA it is not (currently) possible to establish a "
         errMsg += "direct connection"
         raise SqlmapUnsupportedFeatureException(errMsg)
